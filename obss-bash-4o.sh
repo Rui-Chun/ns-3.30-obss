117ee85a447707ff0dfb9da2ad11dbd5e6e6d309
jw27 <jw27@illinois.edu>
2019-10-04 12:33:53 -0500

    change bash tcp-2

--- obss-bash-4o.sh
+++ obss-bash-4o.sh
@@ -17,11 +17,20 @@ do
 done
 
 # tcp-1 
-for ((thr=2;thr<=12;thr+=2))
+for ((thr=2;thr<=1;thr+=2))
 do
-    for ((cnt=1;cnt<=100;cnt++))
+    for ((cnt=1;cnt<=0;cnt++))
     do
         ./waf --run "mesh-loc-jw --rateControl=obss --datarateUp=false --gridSize=4 --apXStep=30 --apYStep=30 --RngSeed=${cnt} --startTime=10 --totalTime=40 --route=static --datarate=${thr}e5 --mac=adhoc --app=tcp --constantRate=HeMcs4 --apNum=16 --gateways=0+4+8+12 --appl=3+7+11+15 --recordPath=./obss-bash-tcp-1/grid4o-${thr}-${cnt}-record.txt" | tee ./obss-bash-tcp-1/grid4o-${thr}-${cnt}.txt
         # cp /home/jiaming/TransRecords.txt ./obss-bash-tcp/grid4o-${cnt}-record.txt
+    done
+done
+
+# tcp-2
+for ((xs=24;xs<=36;xs+=3))
+do
+    for ((ys=24;ys<=36;ys+=3))
+    do
+        ./waf --run "mesh-loc-jw --rateControl=obss --datarateUp=false --gridSize=4 --apXStep=${xs} --apYStep=${ys} --RngSeed=${cnt} --startTime=10 --totalTime=40 --route=static --datarate=6e5 --mac=adhoc --app=tcp --constantRate=HeMcs4 --apNum=16 --gateways=0+4+8+12 --appl=3+7+11+15 --recordPath=./obss-bash-tcp-2/grid4o-${xs}-${ys}-${cnt}-record.txt" | tee ./obss-bash-tcp-2/grid4o-${xs}-${ys}-${cnt}.txt
     done
 done
