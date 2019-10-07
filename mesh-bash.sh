ea5ccbf0e298014a405605a82b1a9f841beb8d73
jw27 <jw27@illinois.edu>
2019-10-07 03:17:52 -0500

    facebook trace tcp olsr/static results

--- mesh-bash.sh
+++ mesh-bash.sh
@@ -2,16 +2,28 @@
 
 ./waf
 
-# udp olsr
+# # udp olsr
+# for ((cnt=$1;cnt<=$2;cnt++))
+# do
+#     ./waf --run "mesh-loc-2 --apNum=26 --datarate=5e5 --app=udp --rndSeed=${cnt} --route=olsr" | tee ./mesh-bash-udp/${cnt}.txt
+# done
+
+# # udp
+# for ((cnt=$1;cnt<=$2;cnt++))
+# do
+#     ./waf --run "mesh-loc-2 --apNum=26 --datarate=3e5 --app=udp --rndSeed=${cnt} --route=static" | tee ./mesh-bash-udps/${cnt}.txt
+# done
+
+# tcp
 for ((cnt=$1;cnt<=$2;cnt++))
 do
-    ./waf --run "mesh-loc-2 --apNum=26 --datarate=5e5 --app=udp --rndSeed=${cnt} --route=olsr" | tee ./mesh-bash-udp/${cnt}.txt
+    ./waf --run "mesh-loc-2 --apNum=26 --datarate=3e5 --app=tcpr --rndSeed=${cnt} --route=olsr" | tee ./mesh-bash-tcp/${cnt}.txt
 done
 
-# udp
+# tcp
 for ((cnt=$1;cnt<=$2;cnt++))
 do
-    ./waf --run "mesh-loc-2 --apNum=26 --datarate=3e5 --app=udp --rndSeed=${cnt} --route=static" | tee ./mesh-bash-udps/${cnt}.txt
+    ./waf --run "mesh-loc-2 --apNum=26 --datarate=3e5 --app=tcpr --rndSeed=${cnt} --route=static" | tee ./mesh-bash-tcps/${cnt}.txt
 done
 
 rm minstrel-*.txt
