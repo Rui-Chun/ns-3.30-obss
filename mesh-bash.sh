0d5fee232c4f43cb32bcdf746f7499d38ee86c8a
jw27 <jw27@illinois.edu>
2019-10-07 02:52:26 -0500

    facebook trace udp olsr/static results

--- mesh-bash.sh
+++ mesh-bash.sh
@@ -5,13 +5,13 @@
 # udp olsr
 for ((cnt=$1;cnt<=$2;cnt++))
 do
-    ./waf --run "mesh-loc-2 --apNum=26 --datarate=5e5 --app=udp --RngSeed=${cnt} --route=olsr" | tee ./mesh-bash-udp/${cnt}.txt
+    ./waf --run "mesh-loc-2 --apNum=26 --datarate=5e5 --app=udp --rndSeed=${cnt} --route=olsr" | tee ./mesh-bash-udp/${cnt}.txt
 done
 
 # udp
 for ((cnt=$1;cnt<=$2;cnt++))
 do
-    ./waf --run "mesh-loc-2 --apNum=26 --datarate=3e5 --app=udp --RngSeed=${cnt} --route=static" | tee ./mesh-bash-udps/${cnt}.txt
+    ./waf --run "mesh-loc-2 --apNum=26 --datarate=3e5 --app=udp --rndSeed=${cnt} --route=static" | tee ./mesh-bash-udps/${cnt}.txt
 done
 
 rm minstrel-*.txt
