382028e0ac9ba01ef8126aa72d1b2d2117f60162
ruichun <ruichun@illinois.edu>
2019-10-08 04:19:20 -0500

    obss 2x3 data 75m

--- obss-new-2x3.sh
+++ obss-new-2x3.sh
@@ -11,11 +11,23 @@
 #     done
 # done
 
-# udp 2x3 change ystep
-for ((ys=72;ys<=90;ys+=3))
+# # udp 2x3 change ystep
+# for ((ys=72;ys<=90;ys+=3))
+# do
+#     for ((cnt=$1;cnt<=$2;cnt++))
+#     do
+#         ./waf --run "mesh-loc-jw --rateControl=obss --datarateUp=false --apXStep=20 --apYStep=$ys --RngSeed=${cnt} --startTime=10 --totalTime=40 --route=static --datarate=14e6 --mac=adhoc --app=udp --constantRate=HeMcs4 --gridSize=3 --apNum=6 --gateways=0+3 --appl=2+5 --recordPath=./obss-bash-2x3-y/grid3o-${ys}-${cnt}-record.txt" | tee ./obss-bash-2x3-y/grid2x3o-${ys}-${cnt}.txt
+#     done
+# done
+
+# udp 2x3 75m
+for ((cnt=$1;cnt<=$2;cnt++))
 do
-    for ((cnt=$1;cnt<=$2;cnt++))
-    do
-        ./waf --run "mesh-loc-jw --rateControl=obss --datarateUp=false --apXStep=20 --apYStep=$ys --RngSeed=${cnt} --startTime=10 --totalTime=40 --route=static --datarate=14e6 --mac=adhoc --app=udp --constantRate=HeMcs4 --gridSize=3 --apNum=6 --gateways=0+3 --appl=2+5 --recordPath=./obss-bash-2x3-y/grid3o-${ys}-${cnt}-record.txt" | tee ./obss-bash-2x3-y/grid2x3o-${ys}-${cnt}.txt
-    done
+    ./waf --run "mesh-loc-jw --rateControl=constant --datarateUp=false --apXStep=20 --apYStep=75 --RngSeed=${cnt} --startTime=10 --totalTime=40 --route=static --datarate=16e6 --mac=adhoc --app=udp --constantRate=HeMcs4 --gridSize=3 --apNum=6 --gateways=0+3 --appl=2+5" | tee ./obss-bash-2x3-75/grid2x3-${cnt}.txt
+done
+
+# udp 2x3 75m
+for ((cnt=$1;cnt<=$2;cnt++))
+do
+    ./waf --run "mesh-loc-jw --rateControl=obss --datarateUp=false --apXStep=20 --apYStep=75 --RngSeed=${cnt} --startTime=10 --totalTime=40 --route=static --datarate=16e6 --mac=adhoc --app=udp --constantRate=HeMcs4 --gridSize=3 --apNum=6 --gateways=0+3 --appl=2+5 --recordPath=./obss-bash-2x3-75/grid3o-${cnt}-record.txt" | tee ./obss-bash-2x3-75/grid2x3o-${cnt}.txt
 done
