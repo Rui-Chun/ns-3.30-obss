37be4ea392022021d69e148447f7d39b10e6005f
Ubuntu <ubuntu@ip-172-31-9-176.us-east-2.compute.internal>
2020-02-16 17:10:54 +0000

    apsta ofdma results

--- ofdma_main.bash
+++ ofdma_main.bash
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-for i in {0..1}
+./waf
+
+for i in {0..5}
 do
-    for j in {0..15}
+    for j in {0..14}
     do
-        echo $((i*16+j))
-        bash ofdma_loop.bash $((i*16+j)) &
+        echo $((i*15+j))
+        bash ofdma_loop.bash $((i*15+j)) &
     done
     wait
 done
